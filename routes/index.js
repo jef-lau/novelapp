aaf8ba2c946e79cee3e52d51ad92b14bf21c104b
Jeff Lau <jel155@ucsd.edu>
2016-02-16 19:07:39 -0800

    added testing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 // Get all of our friend data
 var data = require('../data.json');
+var crown = require('../pilcrows.json');
 
 exports.view = function(req, res){
 	console.log(data);
+	// we need to fix this. we can only render 1 json at time
+	res.render('index', crown);
 	res.render('index', data);
 };
